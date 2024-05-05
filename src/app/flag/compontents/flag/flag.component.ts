3ca2f8d6338d701b9344f7bfea82df400c6b172c
Jean Barbaroux <jean.barbaroux@epita.fr>
2024-05-05 11:43:50 +0200

    add: localstorage to flags

--- src/app/flag/compontents/flag/flag.component.ts
+++ src/app/flag/compontents/flag/flag.component.ts
@@ -1,13 +1,13 @@
 import {Component, ViewChild} from '@angular/core';
 import {CompareCountryComponent} from "../../../country/components/compare-country/compare-country.component";
-import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
-import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {TranslateModule} from "@ngx-translate/core";
 import {FlagService} from "../../../core/services/flag.service";
 import {LangService} from "../../../services/lang.service";
 import {LocalStorageService} from "../../../core/services/local-storage.service";
 import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
-import {MatLabel, MatFormField} from "@angular/material/form-field";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {InputComponent} from "../../../core/components/input/input.component";
 import {CountryService} from "../../../core/services/country.service";
 import {GuessedCountryComponent} from "../../components/guessed-country/guessed-country.component";
@@ -34,21 +34,26 @@ import {GuessedCountryComponent} from "../../components/guessed-country/guessed-
 })
 export class FlagComponent {
   countries: string[] = [];
-  countriesTried: string[] = [];
+  flagsTried: string[] = [];
   countryFound: boolean = false;
   selectedCountry: string = '';
   flagHtml: SafeHtml = '';
+  FlagCount: number = 0;
+
   @ViewChild(InputComponent) inputComponent!: InputComponent;
 
-  constructor(private flagService: FlagService, private langService: LangService, private localStorageService: LocalStorageService, private sanitizer: DomSanitizer ) {
+
+  constructor(private flagService: FlagService, private langService: LangService, private localStorageService: LocalStorageService, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
+    this.flagsTried = this.localStorageService.getItem('flagsTried');
+    this.FlagCount = this.localStorageService.getItem('FlagCount');
     this.localStorageService.resetAtMidnight();
     this.flagService.getAllCountries()
       .subscribe((countryList) => {
         let isLocalStorageEmpty = (this.localStorageService.getItem('allCountries')).length
-        if (isLocalStorageEmpty!== 0) {
+        if (isLocalStorageEmpty !== 0) {
           this.countries = this.localStorageService.getItem('allCountries');
         } else {
           this.countries = countryList.sort();
@@ -63,9 +68,12 @@ export class FlagComponent {
     if (string === '') {
       return
     }
-    this.countriesTried.reverse();
-    this.countriesTried.push(string);
-    this.countriesTried.reverse();
-      this.inputComponent.reset();
+    this.flagsTried.reverse();
+    this.flagsTried.push(string);
+    this.flagsTried.reverse();
+    this.inputComponent.reset();
+    this.FlagCount++;
+    this.localStorageService.setItem('flagsTried', this.flagsTried);
+    this.localStorageService.setItem('FlagCount', this.FlagCount);
   }
 }
